520e075e28206c5359547ac632bf2b37711a9484
unknown <ckr_mm_ckr@163.com>
2023-10-12 14:49:30 +0800

    update

--- Recipe Web/apps/model/model.py
+++ Recipe Web/apps/model/model.py
@@ -4,9 +4,9 @@ from apps import db
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String(80), unique=True)
-    password = db.Column(db.String(32), nullable=False)
-    photo = db.Column(db.String(320), nullable=False)
+    name = db.Column(db.String(255), unique=True)
+    password = db.Column(db.String(255), nullable=False)
+    photo = db.Column(db.String(255), nullable=False)
 
     def __repr__(self):
         return '<User %r>' % self.name
