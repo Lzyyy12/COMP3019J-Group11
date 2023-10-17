47757784baa08ad525e2b43ba7cd4260650c84cd
Catherine <Catherine__Z@163.com>
2023-10-17 15:35:10 +0800

    fix: db creation

--- database/dbfinal.sql
+++ database/dbfinal.sql
@@ -4,14 +4,14 @@ CREATE DATABASE recipe_web CHARACTER SET utf8 COLLATE utf8_general_ci;
 
 USE recipe_web;
 
-DROP TABLE IF EXISTS User;
-CREATE TABLE User (
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
     password VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci,
     photo VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci
 );
 
-INSERT INTO User (username, password)
+INSERT INTO users (username, password)
 VALUES
     ('user', '123');
