2b1cf2a004bf9528139994fe94dbc86fdabcf30a
Lzyyy12 <129343860+Lzyyy12@users.noreply.github.com>
2023-12-10 20:03:05 +0800

    modify admin

--- Recipe Web/apps/static/js/admin.js
+++ Recipe Web/apps/static/js/admin.js
@@ -27,6 +27,30 @@ window.onload = function() {
   }
 }
 
+document.addEventListener('DOMContentLoaded', function() {
+    var tags = document.querySelectorAll('.tags-list a'); // 获取所有标签
+
+    tags.forEach(function(tag) {
+        tag.addEventListener('click', function() {
+            // 首先移除所有按钮的 'active-tag' 类
+            tags.forEach(function(t) {
+                t.classList.remove('active-tag');
+            });
+
+            // 然后给当前点击的按钮添加 'active-tag' 类
+            tag.classList.add('active-tag');
+        });
+    });
+});
+
+function hideNavLinks() {
+  document.querySelector('.nav-links').style.display = 'none';
+}
+function showNavLinks() {
+  document.querySelector('.nav-links').style.display = 'flex';
+}
+
+
 // function search() {
 //   var input = document.getElementById("search").value;
 //   var url = "./api/search?keyword=" + input;
--- Recipe Web/apps/static/js/recipe.js
+++ Recipe Web/apps/static/js/recipe.js
@@ -96,23 +96,3 @@ function uploadimage(username) {
     });
 }
 // 当本地存储发生变化时触发
-window.addEventListener('storage', function(event) {
-  if (event.key === 'theme') {
-    // 检查深夜模式的新值，并相应地更新页面
-    if (event.newValue === 'dark') {
-      document.body.classList.add('dark-mode');
-    } else {
-      document.body.classList.remove('dark-mode');
-    }
-  }
-});
-document.addEventListener('DOMContentLoaded', (event) => {
-  if (localStorage.getItem('theme') === 'dark') {
-    document.body.classList.add('dark-mode');
-    // 如果有切换按钮，也更新其文本
-    var modeText = document.getElementById('toggle-mode');
-    if (modeText) {
-      modeText.textContent = 'Light Mode';
-    }
-  }
-});
